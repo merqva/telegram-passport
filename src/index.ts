8e6d39d003e86f02dc2d6416f0218363f1d0247c
Yoel Navas <yn4v4s@gmail.com>
2022-01-24 21:35:53 -0500

    decrypt credentials

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export { TelegramPassport } from './telegram-passport'
+export { TelegramPassport } from './telegram-passport';
+export { Credentials } from './interfaces/credentials';
+export { DataCredentials } from './interfaces/data-credentials';
+export { EncryptedCredentials } from './interfaces/encrypted-credentials';
+export { FileCredentials } from './interfaces/file-credentials';
+export { SecureData } from './interfaces/secure-data';
+export { SecureValue } from './interfaces/secure-value';
